28667be21e719d97415664ac4bfba4fa9349b845
Alper Ozdamar <Alper.Ozdamar@argela.com.tr>
2019-04-29 12:52:07 -0700

    1-) Private&Public Key Generation  is Completed! 2-) Encrption and Decryption with Pub/Private Key and Verification Completed!

--- main.go
+++ main.go
@@ -1,34 +1,67 @@
 package main
 
 import (
-	"./p3"
-	"log"
-	"net/http"
-	"os"
+	"reflect"
+
+	"./p5"
+	"fmt"
 )
 
 func main() {
 
-//	testJson :="{\"hash\":\"\",\"timeStamp\":0,\"height\":0,\"parentHash\":\"\",\"size\":0,\"mpt\":{}}";
+	//	testJson :="{\"hash\":\"\",\"timeStamp\":0,\"height\":0,\"parentHash\":\"\",\"size\":0,\"mpt\":{}}";
 
-//	newBlock:=p2.Block{}
+	//	newBlock:=p2.Block{}
 
-//	newBlock.DecodeFromJson(testJson)
+	//	newBlock.DecodeFromJson(testJson)
 
+	//	fmt.Println("Alper Block :",newBlock)
+	//	fmt.Println("Alper P2.Block :",p2.Block{})
 
-//	fmt.Println("Alper Block :",newBlock)
-//	fmt.Println("Alper P2.Block :",p2.Block{})
-
-//	mpt1 := p1.MerklePatriciaTrie{}
-//	mpt1.Initial()
-//	block1 := p2.Block{}
-//	block1.Initial(1,"ab",mpt1);
+	//	mpt1 := p1.MerklePatriciaTrie{}
+	//	mpt1.Initial()
+	//	block1 := p2.Block{}
+	//	block1.Initial(1,"ab",mpt1);
 
 	//data.TestPeerListRebalance()
-	router := p3.NewRouter()
-	if len(os.Args) > 1 {
-		log.Fatal(http.ListenAndServe(":" + os.Args[1], router))
-	} else {
-		log.Fatal(http.ListenAndServe(":6686", router))
+
+	//KEY GENERATION
+	key:=p5.GenerateKeyPair(2048);
+
+	fmt.Println("publicKey:",&key.PublicKey)
+	fmt.Println("privateKey::",key)
+
+	plainText := "AlperWillNeverGiveUp"
+	fmt.Println("Before Encrpytion:\n",plainText)
+	plainTextByteArray:=[]byte(plainText)
+	fmt.Println("Before Encrpytion plainTextByteArray:\n",plainTextByteArray)
+
+	//ENCRYPTION
+	ciphertext:=p5.EncryptWithPublicKey(plainTextByteArray,&key.PublicKey);
+	fmt.Println("CipherText:\n",ciphertext)
+	//DECRYPTION
+	decryptedPlaintextByteArray :=p5.DecryptWithPrivateKey(ciphertext,key)
+	fmt.Println("resultPlaintextByteArray:\n",decryptedPlaintextByteArray)
+	resultPlaintext := string(decryptedPlaintextByteArray);
+	fmt.Println("resultPlaintext:\n",resultPlaintext)
+
+	if(reflect.DeepEqual(plainTextByteArray, decryptedPlaintextByteArray)){
+		fmt.Println("YES THEY ARE SAME PERFECT")
+	}else{
+		fmt.Errorf("NO THEY ARE NOT SAME.THERE IS A PROBLEM")
 	}
+
+
+	//router := p3.NewRouter()
+	//if len(os.Args) > 1 {
+	//	log.Fatal(http.ListenAndServe(":"+os.Args[1], router))
+	//} else {
+	//	log.Fatal(http.ListenAndServe(":6686", router))
+	//}
 }
+
+
+
+
+
+
